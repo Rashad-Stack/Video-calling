f5cbd6238dce49524de81b77c3f062221b2bed39
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-05-26 04:27:26 +0600

    feat: enhance video call component with microphone and camera toggle functionality

--- client/src/components/VideoCall.tsx
+++ client/src/components/VideoCall.tsx
@@ -1,16 +1,63 @@
 import useContext from "@/hooks/useContext";
+import { Mic, MicOff, Phone, Video, VideoOff } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
 import VideoContainer from "./VideoContainer";
+import { Button } from "./ui/button";
 
 export default function VideoCall() {
   const { localStream } = useContext();
+  const [isMicOn, setIsMicOn] = useState<boolean>(false);
+  const [isVidOn, setIsVidOn] = useState<boolean>(false);
+
+  const toggleCamera = useCallback(() => {
+    if (localStream) {
+      const videoTracks = localStream.getVideoTracks()[0];
+      videoTracks.enabled = !videoTracks.enabled;
+      setIsVidOn(videoTracks.enabled);
+    }
+  }, [localStream]);
+
+  const toggleMic = useCallback(() => {
+    if (localStream) {
+      const audioTracks = localStream.getAudioTracks()[0];
+      audioTracks.enabled = !audioTracks.enabled;
+      setIsMicOn(audioTracks.enabled);
+    }
+  }, [localStream]);
+
+  useEffect(() => {
+    if (localStream) {
+      const audioTracks = localStream.getAudioTracks()[0];
+      const videoTracks = localStream.getVideoTracks()[0];
+      setIsMicOn(audioTracks.enabled);
+      setIsVidOn(videoTracks.enabled);
+    }
+  }, [localStream]);
+
   return (
-    <div>
+    <div className="my-5 flex flex-col items-center">
+      <div>
+        {localStream && (
+          <VideoContainer
+            isLocalStream={true}
+            stream={localStream}
+            isOnCall={false}
+          />
+        )}
+      </div>
+
       {localStream && (
-        <VideoContainer
-          isLocalStream={true}
-          stream={localStream}
-          isOnCall={false}
-        />
+        <div className="space-x-5 mt-5 max-w-xs mx-auto flex justify-center">
+          <Button onClick={toggleMic}>
+            {isMicOn ? <Mic size={20} /> : <MicOff size={20} />}
+          </Button>
+          <Button onClick={toggleCamera}>
+            {isVidOn ? <Video size={20} /> : <VideoOff size={20} />}
+          </Button>
+          <Button className="bg-red-500">
+            <Phone size={20} className="rotate-138" />
+          </Button>
+        </div>
       )}
     </div>
   );
